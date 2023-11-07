40f8b6c3e78e33958018c7c1a0aa891395d8b3c6
asandoval2313 <asandoval2313@gmail.com>
2023-11-06 20:43:34 -0800

    added gradle files and tests

---
+++ PantryPal/src/pantrypal/PantryPalTests.java
@@ -0,0 +1,44 @@
+package pantrypal;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PantryPalTests {
+    @BeforeEach
+    void setUp() {
+        PantryPal.recipeStorage = new ArrayList<>();
+        Recipe Recipe1 = new Recipe("Recipe", "Description");
+        PantryPal.recipeStorage.add(Recipe1);
+
+    }
+
+    @Test
+    void testAddRecipe() {
+        int FirstSize = PantryPal.recipeStorage.size();
+        String Recipe = "Recipe";
+        String RecipeDescription = "description";
+        PantryPal.recipeStorage.add(new Recipe(Recipe, RecipeDescription));
+
+        boolean recipeAdded = false;
+        for (int i = 0; i < PantryPal.recipeStorage.size(); i++) {
+            Recipe recipe = PantryPal.recipeStorage.get(i);
+            if (recipe.getTitle().equals(Recipe) && recipe.getDescription().equals(RecipeDescription)) {
+                recipeAdded = true;
+            }
+        }
+        int finalSize = PantryPal.recipeStorage.size();
+
+        assertTrue(recipeAdded, "");
+        assertEquals(FirstSize + 1, finalSize, "");
+    }
+}
