12dac0a2e84c797697b03032dabf2d19bcfecf63
asandoval2313 <asandoval2313@gmail.com>
2023-11-11 11:19:14 -0800

    added transcription

--- app/src/main/java/project/PantryPal.java
+++ app/src/main/java/project/PantryPal.java
@@ -10,6 +10,8 @@ import javafx.scene.control.*;
 import javafx.geometry.*;
 import javax.sound.sampled.*;
 import java.io.*;
+import java.net.*;
+import org.json.*;
 
 import java.io.IOException;
 import java.util.*;
@@ -162,11 +164,6 @@ class MainWindow extends BorderPane {
  */
 class SuggestWindowHeader extends HBox {
     private Button returnButton;
-    private Button recordIngredientsButton;
-    private Label recordingLabel;
-    private AudioFormat audioFormat;
-    private TargetDataLine targetDataLine;
-    private boolean recording = false;
 
     SuggestWindowHeader() {
         // Set header appearance
@@ -178,17 +175,53 @@ class SuggestWindowHeader extends HBox {
         titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
         this.setAlignment(Pos.CENTER); // Align the text to the Center
 
-        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
-        String defaultLabelStyle = "-fx-font: 13 arial; -fx-pref-width: 175px; -fx-pref-height: 50px; -fx-text-fill: red; visibility: hidden";
-
+        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF; -fx-font-weight: bold; -fx-font: 11 arial;";
         returnButton = new Button("Return");
         returnButton.setStyle(defaultButtonStyle);
 
+        this.getChildren().addAll(titleText, returnButton);
+    }
+
+    public Button getReturnButton() {
+        return returnButton;
+    }
+}
+
+/**
+ * Center segment for SuggestWindow
+ */
+class SuggestWindowBody extends VBox {
+    private Label ingredientLabel;
+    private TextField ingredientTextField;
+    private Button recordIngredientsButton;
+    private Label recordingLabel;
+    private AudioFormat audioFormat;
+    private TargetDataLine targetDataLine;
+    private boolean recording = false;
+
+    // Whisper API setup
+    private static final String API_ENDPOINT = "https://api.openai.com/v1/audio/transcriptions";
+    private static final String TOKEN = "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";
+    private static final String MODEL = "whisper-1";
+    private static final String FILE_PATH = "/home/adsandov/code/pantrypal/cse-110-project-team-23/app/recording.wav";
+
+    SuggestWindowBody() {
+        this.setStyle("-fx-background-color: #F0F8FF;");
+
+        ingredientLabel = new Label();
+        ingredientLabel.setText("Ingredients");
+        ingredientLabel.setPrefSize(100, 30);
+
+        ingredientTextField = new TextField();
+        ingredientTextField.setPrefSize(400, 50);
+
         recordIngredientsButton = new Button("Record Ingredients");
+        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF; -fx-font-weight: bold; -fx-font: 11 arial;";
         recordIngredientsButton.setStyle(defaultButtonStyle);
 
         recordingLabel = new Label("Recording...");
-        recordingLabel.setStyle(defaultLabelStyle);
+        recordingLabel.setStyle(
+                "-fx-font: 13 arial; -fx-pref-width: 175px; -fx-pref-height: 50px; -fx-text-fill: red; visibility: hidden");
 
         recordIngredientsButton.setOnAction(e -> {
             if (!recording) {
@@ -199,12 +232,22 @@ class SuggestWindowHeader extends HBox {
                 recording = false;
                 recordIngredientsButton.setText("Record Ingredients");
                 stopRecording();
+                ingredientTextField.setText("transcribing...");
+                transcribeAudio();
             }
         });
 
-        this.getChildren().addAll(titleText, returnButton, recordIngredientsButton);
-
         audioFormat = getAudioFormat();
+
+        this.getChildren().addAll(ingredientLabel, ingredientTextField, recordIngredientsButton, recordingLabel);
+    }
+
+    public String getIngredients() {
+        return ingredientTextField.getText();
+    }
+
+    public void clear() {
+        ingredientTextField.clear();
     }
 
     private AudioFormat getAudioFormat() {
@@ -274,41 +317,121 @@ class SuggestWindowHeader extends HBox {
         targetDataLine.close();
     }
 
-    public Button getReturnButton() {
-        return returnButton;
+    private void transcribeAudio() {
+        Thread t = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            // Create file object from file path
+                            File audioFile = new File(FILE_PATH);
+
+                            // Set up HTTP connection
+                            URL url = new URI(API_ENDPOINT).toURL();
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            connection.setRequestMethod("POST");
+                            connection.setDoOutput(true);
+
+                            // Set up request headers
+                            String boundary = "Boundary-" + System.currentTimeMillis();
+                            connection.setRequestProperty(
+                                    "Content-Type",
+                                    "multipart/form-data; boundary=" + boundary);
+                            connection.setRequestProperty("Authorization", "Bearer " + TOKEN);
+
+                            // Set up output stream to write request body
+                            OutputStream outputStream = connection.getOutputStream();
+
+                            // Write model parameter to request body
+                            writeParameterToOutputStream(outputStream, "model", MODEL, boundary);
+
+                            // Write file parameter to request body
+                            writeFileToOutputStream(outputStream, audioFile, boundary);
+
+                            // Write closing boundary to request body
+                            outputStream.write(("\r\n--" + boundary + "--\r\n").getBytes());
+
+                            // Flush and close output stream
+                            outputStream.flush();
+                            outputStream.close();
+
+                            // Get response code
+                            int responseCode = connection.getResponseCode();
+
+                            // Check response code and handle response accordingly
+                            if (responseCode == HttpURLConnection.HTTP_OK) {
+                                handleSuccessResponse(connection);
+                            } else {
+                                handleErrorResponse(connection);
+                            }
+
+                            // Disconnect connection
+                            connection.disconnect();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        t.start();
     }
 
-    public Button getRecordIngredientsButton() {
-        return recordIngredientsButton;
-    }
-}
-
-/**
- * Center segment for SuggestWindow
- */
-class SuggestWindowBody extends VBox {
-    private Label ingredientLabel;
-    private TextField ingredientTextField;
-
-    SuggestWindowBody() {
-        this.setStyle("-fx-background-color: #F0F8FF;");
-
-        ingredientLabel = new Label();
-        ingredientLabel.setText("Ingredients");
-        ingredientLabel.setPrefSize(100, 30);
-
-        ingredientTextField = new TextField();
-        ingredientTextField.setPrefSize(400, 50);
-
-        this.getChildren().addAll(ingredientLabel, ingredientTextField);
+    // Helper method to write a parameter to the output stream in multipart form
+    // data format
+    private static void writeParameterToOutputStream(
+            OutputStream outputStream,
+            String parameterName,
+            String parameterValue,
+            String boundary) throws IOException {
+        outputStream.write(("--" + boundary + "\r\n").getBytes());
+        outputStream.write(
+                ("Content-Disposition: form-data; name=\"" + parameterName + "\"\r\n\r\n").getBytes());
+        outputStream.write((parameterValue + "\r\n").getBytes());
     }
 
-    public String getIngredients() {
-        return ingredientTextField.getText();
+    // Helper method to write a file to the output stream in multipart form data
+    // format
+    private static void writeFileToOutputStream(
+            OutputStream outputStream,
+            File file,
+            String boundary) throws IOException {
+        outputStream.write(("--" + boundary + "\r\n").getBytes());
+        outputStream.write(
+                ("Content-Disposition: form-data; name=\"file\"; filename=\"" +
+                        file.getName() +
+                        "\"\r\n").getBytes());
+        outputStream.write(("Content-Type: audio/mpeg\r\n\r\n").getBytes());
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+        fileInputStream.close();
     }
 
-    public void clear() {
-        ingredientTextField.clear();
+    private void handleSuccessResponse(HttpURLConnection connection)
+            throws IOException, JSONException {
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        JSONObject responseJson = new JSONObject(response.toString());
+
+        String transcribedText = responseJson.getString("text");
+
+        // update text field
+        ingredientTextField.setText(transcribedText);
+    }
+
+    private void handleErrorResponse(HttpURLConnection connection)
+            throws IOException, JSONException {
+        // Handle error response as needed
     }
 }
 
